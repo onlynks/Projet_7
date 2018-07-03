d2905c121dec4f13e53a906f37115079c93c0eba
onlynks <nicogar12@gmail.com>
2018-07-03 15:03:22 +0200

    facebook api implemented

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -19,6 +19,7 @@ class AppKernel extends Kernel
             new AppBundle\AppBundle(),
             new JMS\SerializerBundle\JMSSerializerBundle(),
             new WhiteOctober\PagerfantaBundle\WhiteOctoberPagerfantaBundle(),
+            new EightPoints\Bundle\GuzzleBundle\GuzzleBundle()
             
         ];
 
--- src/AppBundle/Controller/UserController.php
+++ src/AppBundle/Controller/UserController.php
@@ -6,14 +6,24 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use GuzzleHttp\Client;
 
 class UserController extends Controller
 {
     /**
      * @Route("/login", name="login")
      */
-    public function loginAction()
+    public function loginAction(Request $request)
     {
+        $token = $request->headers->get('X-AUTH-TOKEN');
+
+        $client = new Client([
+            'base_uri'=>'https://graph.facebook.com/me?access_token='.$token
+        ]);
+
+        $response = $client->request('GET');
+
+        return new Response($response->getBody()->getContents());
     }
 
     public function createUserAction()
@@ -31,4 +41,14 @@ class UserController extends Controller
     public function listUserAction()
     {}
 
+    /**
+     * @Route("/test", name="test")
+     */
+    public function testAction(Request $request)
+    {
+
+        $test = $request->headers->get('test');
+var_dump($test);
+        return new Response($test);
+    }
 }
